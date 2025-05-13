1b28b33f1b5c5e3e14491cde6166106be94d7180
FrancisMurayaDev <francismurayadev@gmail.com>
2025-05-14 01:19:25 +0300

    practicing dom

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
 `use strict`;
-console.log(document.querySelector(".message"));
+// document.querySelector('.message').textContent = '🎊Correct Number!';
+// document.querySelector('.number').textContent = 17;
+// document.querySelector('.guess').value = 19;
+// document.querySelector('.score').textContent = 7;
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
