11cad1b4d9643323bea1833d90a2a5101cc5f700
FrancisMurayaDev <francismurayadev@gmail.com>
2025-05-14 02:25:00 +0300

    build logic

--- scripts.js
+++ scripts.js
@@ -4,6 +4,35 @@
 // document.querySelector('.guess').value = 19;
 // document.querySelector('.score').textContent = 7;
 
+const secretNumber = Math.floor(Math.random() * 20 + 1);
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⚠️No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎊 Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You Lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You Lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
